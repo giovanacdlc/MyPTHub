c69d8e0963f5ff674e43547259a81c066f78ad14
Giovana De La Cruz <giovanacdlc@hotmail.com>
2023-05-30 15:47:15 -0700

    Register form now connected to the backend

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,87 +2,123 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 function Register() {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     username: '',
-//     password: '',
-//   });
-  const [formData, setFormData] = useState();
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const { name, username, password } = formData;
-//   const onChange = (e) => {
-//     setFormData((prevState) => ({
-//       ...prevState,
-//       [e.target.name]: e.target.value,
-//     }));
-//   };
-  const userObject = {
-    name: name,
-    username: username,
-    password: password
-  }
+    // Create an object with the data
+    const data = {
+      name: name,
+      username: username,
+      password: password,
+    };
 
-  const onSubmit = (e) => {
-    //when the user info is submitted, it should trigger a fetch post request to the backend
-    // e.preventDefault();
-    async function createUser() {
-      try {
-        const response = await fetch('api/users', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(userObject),
-        });
-        const data = await response.json();
-        console.log(data);
-        setFormData(data);
-      } catch (err) {
-        console.log(err);
+    try {
+      // Make the POST request
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      console.log(data);
+      // Check if the request was successful
+      if (response.ok) {
+        // Request succeeded, do something with the response
+        console.log('Data sent successfully!');
+      } else {
+        // Request failed, handle the error
+        console.error('Error:', response.status);
       }
+    } catch (error) {
+      console.error('Error:', error);
     }
-    createUser();
   };
 
   return (
     <div className="container">
       <h1>Please create an account</h1>
-
-      <section>
-        <form>
-          <input
-            className="form-control"
+      <form onSubmit={handleSubmit}>
+        <label>
+          Full Name:
+          <input className="form-control"
             type="text"
-            id="name"
-            name="name"
             value={name}
             placeholder="Enter your name"
-            onChange={onChange}
+            onChange={(e) => setName(e.target.value)}
           />
-          <input
-            className="form-control"
+        </label>
+        <br />
+        <label>
+          Username:
+          <input className="form-control"
             type="text"
-            id="username"
-            name="username"
             value={username}
             placeholder="Username"
-            onChange={onChange}
+            onChange={(e) => setUsername(e.target.value)}
           />
-          <input
-            className="form-control"
+        </label>
+        <br />
+        <label>
+          Password:
+          <input className="form-control"
             type="password"
-            id="password"
-            name="password"
             value={password}
             placeholder="Password"
-            onChange={onChange}
+            onChange={(e) => setPassword(e.target.value)}
           />
-        </form>
-        <button type="submit" className="btn" onClick={() => onSubmit()}>
-          Submit
-        </button>
-      </section>
+        </label>
+        <br />
+        <button type="submit" className='btn'>Submit</button>
+      </form>
     </div>
   );
 }
+
+//   return (
+//     <div className="container">
+//       <h1>Please create an account</h1>
+
+//       <section>
+//         <form>
+//           <input
+//             className="form-control"
+//             type="text"
+//             id="name"
+//             name="name"
+//             value={name}
+//             placeholder="Enter your name"
+//             onChange={onChange}
+//           />
+//           <input
+//             className="form-control"
+//             type="text"
+//             id="username"
+//             name="username"
+//             value={username}
+//             placeholder="Username"
+//             onChange={onChange}
+//           />
+//           <input
+//             className="form-control"
+//             type="password"
+//             id="password"
+//             name="password"
+//             value={password}
+//             placeholder="Password"
+//             onChange={onChange}
+//           />
+//         </form>
+//         <button type="submit" className="btn" onClick={() => onSubmit()}>
+//           Submit
+//         </button>
+//       </section>
+//     </div>
+//   );
+// }
 
 export default Register;
